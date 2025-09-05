fb692c5086243f57302ecb1b82cfb44f397e4b49
unibna <nguyenduylan711@gmail.com>
2025-09-06 00:41:25 +0700

    add health check

--- app/apis/health.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-
-router = APIRouter()
-
-
-@router.get("/health-check")
-async def health_check():
-    return {"status": "ok"}
--- app/core/app.py
+++ app/core/app.py
@@ -8,8 +8,6 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.core.logging import configure_logging, get_logger
 from app.apis.router import router as api_router
-from app.integrations.redis import RedisClient
-from app.integrations.mqtt import MqttClient
 
 
 logger = get_logger()
@@ -36,15 +34,10 @@ async def lifespan(_app: FastAPI) -> AsyncGenerator[None, None]:
 def bootstrap_services():
     logger.info("starting", app=settings.app_name)
     init_timezone_and_logging()
-    # init_redis()
-    # init_mqtt()
-    # start_mqtt()
 
 
 def shutdown_services():
     logger.info("stopped", app=settings.app_name)
-    # stop_mqtt()
-    # close_redis()
 
 
 def init_timezone_and_logging():
@@ -54,27 +47,3 @@ def init_timezone_and_logging():
     except AttributeError:
         pass
     configure_logging(settings.log_level)
-
-
-def init_redis():
-    RedisClient.init(settings.redis_url)
-
-
-def close_redis():
-    RedisClient.close()
-
-
-def init_mqtt():
-    MqttClient.init(
-        host=settings.mqtt_host,
-        port=settings.mqtt_port,
-        topic_prefix=settings.topic_prefix,
-    )
-
-
-def start_mqtt():
-    MqttClient.start()
-
-
-def stop_mqtt():
-    MqttClient.stop()

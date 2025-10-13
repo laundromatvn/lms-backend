3ea9800335a883feaaa666f87d14e174b5cbba4c
unibna <nguyenduylan711@gmail.com>
2025-10-13 17:14:55 +0700

    enhance otp email

--- app/tasks/auth/send_otp_task.py
+++ app/tasks/auth/send_otp_task.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from app.core.celery_app import celery_app
 from app.core.logging import logger
 
@@ -7,7 +9,8 @@ from app.operations.auth.send_otp_operation import SendOTPOperation
 @celery_app.task(name="app.tasks.auth.send_otp_task")
 def send_otp_task(email: str):
     logger.info(f"Sending OTP to {email}")
-    SendOTPOperation.execute(email)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(SendOTPOperation.execute(email))
     logger.info(f"OTP sent to {email}")
 
 

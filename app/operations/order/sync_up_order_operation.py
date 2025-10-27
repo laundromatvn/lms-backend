fc4327739a2f6ea73d3831535bca719229a1c0ff
unibna <nguyenduylan711@gmail.com>
2025-10-27 18:07:58 +0700

    update order details

--- app/operations/order/sync_up_order_operation.py
+++ app/operations/order/sync_up_order_operation.py
@@ -1,6 +1,7 @@
 from typing import List
 import uuid
 
+import sqlalchemy as sa
 from sqlalchemy.orm import Session
 
 from app.core.logging import logger
@@ -20,7 +21,7 @@ class SyncUpOrderOperation:
         logger.info(f"Syncing up order {order_id}")
         
         order = cls.__get_order(db, order_id)
-        
+
         if order.status == OrderStatus.WAITING_FOR_PAYMENT:
             cls.__sync_up_waiting_for_payment(db, order)
         elif order.status == OrderStatus.IN_PROGRESS:
@@ -28,11 +29,11 @@ class SyncUpOrderOperation:
         else:
             logger.info(f"Order {order_id} is not in progress or waiting for payment")
             return
-        
+
     @classmethod
     def __get_order(cls, db: Session, order_id: uuid.UUID):
         return db.query(Order).get(order_id)
-    
+
     @classmethod
     def __sync_up_waiting_for_payment(cls, db: Session, order: Order):
         payments = order.payments
@@ -40,12 +41,27 @@ class SyncUpOrderOperation:
             logger.info(f"No payments found for order {order.id}")
             return
 
+        is_payment_failed = False
         for payment in payments:
             if payment.status in [PaymentStatus.FAILED, PaymentStatus.CANCELLED]:
-                order.status = OrderStatus.PAYMENT_FAILED
-                db.add(order)
-                db.commit()
-                return
+                is_payment_failed = True
+                break
+        
+        if not is_payment_failed:
+            return
+
+        order_details = (
+            db.query(OrderDetail).filter(OrderDetail.order_id == order.id).all()
+        )
+        for order_detail in order_details:
+            order_detail.status = OrderDetailStatus.CANCELLED
+            db.add(order_detail)
+
+        order.status = OrderStatus.PAYMENT_FAILED
+        db.add(order)
+        db.commit()
+        return
+
 
     @classmethod
     def __sync_up_in_progress(cls, db: Session, order: Order):
@@ -65,7 +81,7 @@ class SyncUpOrderOperation:
                 order_detail.status = OrderDetailStatus.FINISHED
                 db.add(order_detail)
         db.commit()
-        
+
         if all(order_detail.status == OrderDetailStatus.FINISHED for order_detail in order.order_details):
             order.status = OrderStatus.FINISHED
             db.add(order)

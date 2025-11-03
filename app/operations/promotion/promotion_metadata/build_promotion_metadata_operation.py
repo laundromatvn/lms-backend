44b5767e1fc940aa7fdb7e35797296a357959e75
unibna <nguyenduylan711@gmail.com>
2025-11-03 18:50:37 +0700

    manage rbac on condition metadata

--- app/operations/promotion/promotion_metadata/build_promotion_metadata_operation.py
+++ app/operations/promotion/promotion_metadata/build_promotion_metadata_operation.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from app.models.user import User
+from app.models.user import User, UserRole
 from app.schemas.promotion.promotion import PromotionMetadata
 from app.schemas.promotion.metadata import (
     CONDITION_METADATA, REWARD_METADATA, LIMIT_METADATA,
@@ -25,6 +25,9 @@ class BuildPromotionMetadataOperation:
         conditions = []
 
         for meta in CONDITION_METADATA:
+            if not self._has_permission(self.current_user.role, meta.allowed_roles):
+                continue
+
             registered_builder = PromotionConditionBuilderRegistry.get_builder(meta.condition_type)
 
             if registered_builder:
@@ -42,3 +45,14 @@ class BuildPromotionMetadataOperation:
 
     async def _build_limits(self) -> List[LimitMetadata]:
         return LIMIT_METADATA
+
+    def _has_permission(
+        self,
+        current_user_role: UserRole,
+        allowed_roles: List[UserRole] | None = None,
+    ) -> bool:
+        
+        if not allowed_roles:
+            raise ValueError("allowed_roles is required")
+
+        return current_user_role in allowed_roles
--- app/schemas/promotion/metadata/condition_metadata.py
+++ app/schemas/promotion/metadata/condition_metadata.py
@@ -1,24 +1,28 @@
 from typing import Any, List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from app.enums.promotion.condition_type import ConditionType
 from app.enums.promotion.operator import Operator
+from app.models.user import UserRole
 
 
 class ConditionMetadata(BaseModel):
     condition_type: ConditionType
     operators: List[Operator]
     options: List[Any] | None = None
+    allowed_roles: List[UserRole] = Field(default_factory=list, exclude=True)
 
 
 CONDITION_METADATA: List[ConditionMetadata] = [
     ConditionMetadata(
         condition_type=ConditionType.TENANTS,
-        operators=[Operator.IN, Operator.NOT_IN]
+        operators=[Operator.IN, Operator.NOT_IN],
+        allowed_roles=[UserRole.ADMIN],
     ),
     ConditionMetadata(
         condition_type=ConditionType.STORES,
-        operators=[Operator.IN, Operator.NOT_IN]
+        operators=[Operator.IN, Operator.NOT_IN],
+        allowed_roles=[UserRole.ADMIN, UserRole.TENANT_ADMIN],
     )
 ]
